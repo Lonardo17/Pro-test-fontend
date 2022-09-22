4fe3380f1889ea458a1b804e86927c3d7dada62f
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-09-22 10:56:06 +0300

    not-finally-created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,33 @@
+import {lazy,  Suspense } from 'react';
+import {
+  Route,
+  Routes,
+  
+} from 'react-router-dom';
+import routes from '../utils/router';
+const { home, auth, test, results, materials, contacts} = routes;
+const AuthView = lazy(() => import('../views/AuthView/AuthView'));
+const HomeView = lazy(() => import('../views/HomeView/HomeView'));
+const ContactsView = lazy(() => import('../views/ContactsView/ContactsView'));
+const MaterialsViews = lazy(() => import('../views/MaterialsView/MaterialsViews'));
+const TestViews = lazy(() => import('../views/TestView/TestViews'));
+const ResultViews = lazy(() => import('../views/ResultView/ResultViews'));
 export const App = () => {
   return (
-    null
+    // <Header/>
+    <div>
+    <Suspense fallback={null}>
+      { false && <AuthView />}
+      {true && (
+        <Routes>
+          <Route exact path={home} element={<HomeView />} />
+          <Route path={contacts} element={<ContactsView />} />
+          <Route path={materials} element={<MaterialsViews />} />
+          <Route path={test} element={<TestViews/>}/>
+          <Route path={results} element={<ResultViews />} />
+        </Routes>
+      )}
+      </Suspense>
+      </div>
   );
 };
---
+++ src/views/AuthView/AuthView.js
@@ -0,0 +1,7 @@
+export default function AuthForm() { 
+    return (
+        <>
+            <h1>hi</h1>
+        </>
+    )
+}
