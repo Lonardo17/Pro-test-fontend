800b74c04becf874f59d40f03bdc73af2b6dda16
Lonardo17 <98488112+Lonardo17@users.noreply.github.com>
2022-09-26 07:51:58 +0000

    Deploying to gh-pages from @ Lonardo17/Pro-test-fontend@77c9f7307b35156a348214720377f8e4ada784e1 🚀

--- static/js/92.b107cd8e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpro_test=self.webpackChunkpro_test||[]).push([[92],{9092:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r=t(184);function u(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"hi"})})}}}]);
-//# sourceMappingURL=92.b107cd8e.chunk.js.map
